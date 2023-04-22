8921f1c5caa02b4c53276293860ad96fb50c1dc0
David Elias Gonzalez Garcia <elias10david@hotmail.com>
2023-04-21 19:49:25 -0600

    [FEATURE][MISHI][COMPLETED] Se termino el comando de juego: mishi
    Habia errores en la funcion que verificaba el ganador, estaba mal escroto. De igual manera, se agregaron char-arts cuando los jugadores ganan, y cuando deciden salir del juego.

--- comandos/mishi.sh
+++ comandos/mishi.sh
@@ -6,7 +6,7 @@
 
 # Shows the board
 mostrar_tablero() {
-    
+    clear
     echo ""
     echo "==============================================="
     echo "***********************************************"
@@ -21,20 +21,22 @@ mostrar_tablero() {
 
 # A function that verifies if there is a winner.
 hay_ganador() {
-    if [ "${tablero[0]}" == "$1" ] && [ "${tablero[1]}" == "$1" ] && [ "${tablero[2]}" == "$1" ] ||
-        [ "${tablero[3]}" == "$1" ] && [ "${tablero[4]}" == "$1" ] && [ "${tablero[5]}" == "$1" ] ||
-        [ "${tablero[6]}" == "$1" ] && [ "${tablero[7]}" == "$1" ] && [ "${tablero[8]}" == "$1" ] ||
-        [ "${tablero[0]}" == "$1" ] && [ "${tablero[3]}" == "$1" ] && [ "${tablero[6]}" == "$1" ] ||
-        [ "${tablero[1]}" == "$1" ] && [ "${tablero[4]}" == "$1" ] && [ "${tablero[7]}" == "$1" ] ||
-        [ "${tablero[2]}" == "$1" ] && [ "${tablero[5]}" == "$1" ] && [ "${tablero[8]}" == "$1" ] ||
-        [ "${tablero[0]}" == "$1" ] && [ "${tablero[4]}" == "$1" ] && [ "${tablero[8]}" == "$1" ] ||
-        [ "${tablero[2]}" == "$1" ] && [ "${tablero[4]}" == "$1" ] && [ "${tablero[6]}" == "$1" ];
+    if [[ "${tablero[0]}" == "$1" && "${tablero[1]}" == "$1" && "${tablero[2]}" == "$1" ]] ||
+        [[ "${tablero[3]}" == "$1" && "${tablero[4]}" == "$1" && "${tablero[5]}" == "$1" ]] ||
+        [[ "${tablero[6]}" == "$1" && "${tablero[7]}" == "$1" && "${tablero[8]}" == "$1" ]] ||
+        [[ "${tablero[0]}" == "$1" && "${tablero[3]}" == "$1" && "${tablero[6]}" == "$1" ]] ||
+        [[ "${tablero[1]}" == "$1" && "${tablero[4]}" == "$1" && "${tablero[7]}" == "$1" ]] ||
+        [[ "${tablero[2]}" == "$1" && "${tablero[5]}" == "$1" && "${tablero[8]}" == "$1" ]] ||
+        [[ "${tablero[0]}" == "$1" && "${tablero[4]}" == "$1" && "${tablero[8]}" == "$1" ]] ||
+        [[ "${tablero[2]}" == "$1" && "${tablero[4]}" == "$1" && "${tablero[6]}" == "$1" ]];
     then
-        return 1 # Return 1 if there is a winner
+        return 0 # Return 0 if there is a winner
     else
-        return 0 # HAHA, LOSER
+        return 1 # HAHA, LOSER
     fi
 }
+
+
 
 # Basically the main function of THE GAME.
 jugar_gato() {
@@ -44,7 +46,7 @@ jugar_gato() {
     mostrar_tablero
     # Main loop of the game.
     while true; do
-        # Player 1 (X) turn
+        # Player 1 (X) turn -------------------------------------------------------------------
         read -p "Es el turno del jugador 1 (X). Seleccione una casilla (0-8): " casilla
         # The next while is for enter on a loop to request the user a valid input.
         while [ "${tablero[$casilla]}" = "X" ] || [ "${tablero[$casilla]}" = "O" ]; do
@@ -52,35 +54,80 @@ jugar_gato() {
         done
         tablero[$casilla]="X" # for asign in that tile the X mark.
         mostrar_tablero # Shows how is goin' the game.
-        if hay_ganador "X"; then # Checks if player X have won.
-            echo "¡El jugador 1 (X) ha ganado!"
+        if todas_las_casillas_estan_ocupadas = 0; then # Checks if there is a tile left to place, if not, ends THE GAME.
+            echo "Ambos perdieron :C, que nubs. "
             break
+        else
+            if hay_ganador "X"; then # Checks if player X have won.
+                echo "¡El jugador 1 (X) ha ganado!"
+                echo "Congratulaciones, mai friendo! :D " # Gano P1, wow, que novedad.
+                echo "      ⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⡿⣿⣇⠀⠀⠀⠀"
+                echo "      ⣿⢏⣹⣳⣯⣗⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⡿⠃⠒⣜⣮⢧⡀⠀⠀"
+                echo "      ⣿⡞⠁⡉⠙⠻⣷⣿⢦⣤⣤⣶⣶⣶⣶⣶⣶⣾⣿⡿⠋⠀⠌⡐⠈⢿⣿⣣⠀⠀"
+                echo "      ⣿⠀⢂⠐⡁⢂⣬⣿⣿⢫⠉⠀⠀⠀⠀⠀⠀⠜⡹⢿⣿⣿⣶⣶⣤⣈⣿⣷⣗⠀"
+                echo "      ⡇⢀⣦⣼⣾⣿⣿⣿⡭⡃⠌⠀⠀⠀⠀⠀⠀⠀⠑⡹⣚⢿⣿⣿⣿⣿⣿⣿⣼⠀"
+                echo "      ⣿⣿⣿⣿⣿⣿⣟⢧⢃⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠉⢎⠳⢯⡟⣿⣻⢿⣯⡷"
+                echo "      ⣿⣿⡿⣟⡿⡓⢎⠂⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣰⣬⣧⡝⢊⠙⣷"
+                echo "      ⠟⢧⠛⠥⠃⢉⠀⣴⣾⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⠀⠀⢸"
+                echo "      ⠈⠄⡈⠤⣁⠢⡀⢿⣿⣿⣿⠃⠀⠀⠀⠀⢠⡄⠀⣴⠀⠀⡀⢙⢛⡛⠭⢠⠃⢆"
+                echo "      ⠐⡠⢑⡒⡄⠓⡌⣌⢩⣩⠷⠶⣤⠀⠀⠀⠀⠳⠾⠃⢀⢸⡼⠋⠋⠛⢦⡃⠞⡠"
+                echo "      ⢀⠱⡈⢖⡈⢣⠜⣠⠟⠀⠀⠀⠀⢳⡄⠀⠀⠀⠀⠀⠐⣾⠁⠀⠀⠀⠈⢧⢣⢸"
+                echo "      ⣆⠠⢑⠢⣉⠆⢼⡟⠀⠀⠀⠀⠀⠈⣷⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠈⣷⢯"
+                echo "      ⡏⠀⠀⢁⠂⢌⡟⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠈⣗⠀⠀⠀⠀⠀⠀⠈⢯"
+                echo "      ⠀⠀⠀⠀⠀⠋⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⢿⡀⠀⠀⠀⠀⠀⠀⠀"
+                echo "      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡇⠀⠀⠀⠀⠀⠀⠘⣷⠀⠀⠀⠀⠀⠀⠀"
+                echo ""
+                break
+            fi
         fi
-        # Player 2 (O) turn
+        # Player 2 (O) turn -------------------------------------------------------------------
         read -p "Es el turno del jugador 2 (O). Seleccione una casilla (0-8): " casilla
         while [ "${tablero[$casilla]}" = "X" ] || [ "${tablero[$casilla]}" = "O" ]; do
             read -p "Casilla ocupada. Seleccione otra casilla: " casilla
         done
         tablero[$casilla]="O" # Marks the O sign on the board (Player 2)
         mostrar_tablero # Show how it is goin THE GAME
-        if hay_ganador "O"; then # Check if P2 have won
-            echo "¡El jugador 2 (O) ha ganado!"
+        
+        if todas_las_casillas_estan_ocupadas = 0; then  # Checks if there is a tile left to place, if not, ends THE GAME.
+            echo "Ambos perdieron :C, que nubs. "
             break
+        else
+            if hay_ganador "O"; then # Check if P2 have won
+            echo "¡El jugador 2 (O) ha ganado!"
+            echo "Congratulaciones, mai friendo! :D " # Todo un crack el P2, mi heroe (It means that P2 is cooler than P1)
+            echo "      ⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⡿⣿⣇⠀⠀⠀⠀"
+            echo "      ⣿⢏⣹⣳⣯⣗⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⡿⠃⠒⣜⣮⢧⡀⠀⠀"
+            echo "      ⣿⡞⠁⡉⠙⠻⣷⣿⢦⣤⣤⣶⣶⣶⣶⣶⣶⣾⣿⡿⠋⠀⠌⡐⠈⢿⣿⣣⠀⠀"
+            echo "      ⣿⠀⢂⠐⡁⢂⣬⣿⣿⢫⠉⠀⠀⠀⠀⠀⠀⠜⡹⢿⣿⣿⣶⣶⣤⣈⣿⣷⣗⠀"
+            echo "      ⡇⢀⣦⣼⣾⣿⣿⣿⡭⡃⠌⠀⠀⠀⠀⠀⠀⠀⠑⡹⣚⢿⣿⣿⣿⣿⣿⣿⣼⠀"
+            echo "      ⣿⣿⣿⣿⣿⣿⣟⢧⢃⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠉⢎⠳⢯⡟⣿⣻⢿⣯⡷"
+            echo "      ⣿⣿⡿⣟⡿⡓⢎⠂⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣰⣬⣧⡝⢊⠙⣷"
+            echo "      ⠟⢧⠛⠥⠃⢉⠀⣴⣾⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⠀⠀⢸"
+            echo "      ⠈⠄⡈⠤⣁⠢⡀⢿⣿⣿⣿⠃⠀⠀⠀⠀⢠⡄⠀⣴⠀⠀⡀⢙⢛⡛⠭⢠⠃⢆"
+            echo "      ⠐⡠⢑⡒⡄⠓⡌⣌⢩⣩⠷⠶⣤⠀⠀⠀⠀⠳⠾⠃⢀⢸⡼⠋⠋⠛⢦⡃⠞⡠"
+            echo "      ⢀⠱⡈⢖⡈⢣⠜⣠⠟⠀⠀⠀⠀⢳⡄⠀⠀⠀⠀⠀⠐⣾⠁⠀⠀⠀⠈⢧⢣⢸"
+            echo "      ⣆⠠⢑⠢⣉⠆⢼⡟⠀⠀⠀⠀⠀⠈⣷⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠈⣷⢯"
+            echo "      ⡏⠀⠀⢁⠂⢌⡟⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠈⣗⠀⠀⠀⠀⠀⠀⠈⢯"
+            echo "      ⠀⠀⠀⠀⠀⠋⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⢿⡀⠀⠀⠀⠀⠀⠀⠀"
+            echo "      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡇⠀⠀⠀⠀⠀⠀⠘⣷⠀⠀⠀⠀⠀⠀⠀"
+            echo ""
+            break
+            fi
         fi
     done
 }
 
-function todas_las_casillas_estan_ocupadas {
-    local ocupadas=0
-    for i in {1..9}; do
-        if [ "${tablero[$i]}" = "X" ] || [ "${tablero[$i]}" = "O" ]; then
-            (( ocupadas++ ))
+todas_las_casillas_estan_ocupadas() {   # Verifies if there is a tile in blank.
+    local ocupadas=0    # Using a counter named "ocupadas"
+    for i in {0..8}; do     # We will go and scan all over the board (tablero[])
+        if [ "${tablero[$i]}" = "X" ] || [ "${tablero[$i]}" = "O" ]; then   # So, if encounters an "X" or "O"
+            (( ocupadas++ ))    # Will increment by 1 the variable "ocupadas"
         fi
     done
-    if [ $ocupadas -eq 9 ]; then
-        return 0
+    if [ $ocupadas -eq 9 ]; then # Till "ocupadas" equals to 9, means that all the board is full.
+        return 0    # Returns 0 if is indeed full.
     else
-        return 1
+        return 1    # Returns 1 if it is still free space.
     fi
 }
 
@@ -125,8 +172,18 @@ main() {
         jugar_gato
         ;;
     2)
-        echo "Has escogido el camino aburrido :C, que aguado"
-        echo " Baiiii! :D "
+        echo "                     /^--^\     /^--^\     /^--^\                        "
+        echo "                     \____/     \____/     \____/                        "
+        echo "                    /      \   /      \   /      \                       "
+        echo "                   |        | |        | |        |                     "
+        echo "                    \__  __/   \__  __/   \__  __/                       "
+        echo "|^|^|^|^|^|^|^|^|^|^|^|^\ \^|^|^|^/ /^|^|^|^|^\ \^|^|^|^|^|^|^|^|^|^|^|^|"
+        echo "| | | | | | | | | | | | |\ \| | |/ /| | | | | | \ \ | | | | | | | | | | |"
+        echo "########################/ /######\ \###########/ /#######################"
+        echo "| | | | | | | | | | | | \/| | | | \/| | | | | |\/ | | | | | | | | | | | |"
+        echo "|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|"
+        echo "            Has escogido el camino aburrido :C, que aguado"
+        echo "                             Baiiii! :D "
         ;;
     *)
         echo " Usted no aprende...... OPCION INCORRECTA " ;;
