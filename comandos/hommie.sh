db8b59bb800da1e7cb081db55bb9fc56cdb21454
David Elias Gonzalez Garcia <elias10david@hotmail.com>
2023-04-22 05:14:10 -0600

    ...

--- comandos/hommie.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Authors: Gonzalez Garcia David Elias and Cynthia Berenice
-# 
-# This command simulates a functional terminal for LINUX
-# Este comando es super cul! :D
-echo "HOLOOOOOOOOOOOOO"
